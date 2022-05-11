cc46c40d7e513b6a86a6938a4d1d9a038ff4abee
ciokma <53948541+ciokma@users.noreply.github.com>
2022-05-11 14:23:29 -0600

    cambio

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello Uday",
+            "message": "hello Mauricio",
             # "location": ip.text.replace("\n", "")
         }),
     }
